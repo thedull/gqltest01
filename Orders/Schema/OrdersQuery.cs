bb54ace64707ca7a8b76cfb35b9c1bfa3881949c
Gabby Tee <gabbytee@NewMew.local>
2018-11-09 23:38:43 -0500

    Added query with arguments

--- Orders/Schema/OrdersQuery.cs
+++ Orders/Schema/OrdersQuery.cs
@@ -6,13 +6,26 @@ namespace Orders.Schema
 {
     public class OrdersQuery : ObjectGraphType<object>
     {
-        public OrdersQuery(IOrderService orders)
+        public OrdersQuery(IOrderService orders, ICustomerService customers)
         {
             Name = "Query";
             Field<ListGraphType<OrderType>>(
                 "orders",
                 resolve: context => orders.GetOrdersAsync()
             );       
+            Field<CustomerType>(
+                "customer",
+                arguments: new QueryArguments(new QueryArgument<IntGraphType> {Name = "id"}),
+                resolve: context => 
+                {
+                    var id = context.GetArgument<int>("id");
+                    return customers.GetCustomerByIdAsync(id);
+                }
+            );
+            Field<ListGraphType<CustomerType>>(
+                "customers",
+                resolve: context => customers.GetCustomersAsync()
+            );
         }
     }
 }
